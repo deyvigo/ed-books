751d8e952a24561e269508466fc6b5fe482abdb8
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-05-22 02:37:48 -0500

    correciones nombres de la bd + avance auto insert de libros

--- src/controllers/book.py
+++ src/controllers/book.py
@@ -10,5 +10,5 @@ class BookController:
   @staticmethod
   def regist_one_book():
     data = request.json
-    response = BookModel().post_one_book(data.get('autor'), data.get('description'), data.get('url_img'))
+    response = BookModel().post_one_book(data.get('name'), data.get('author'), data.get('description'), data.get('url_img'))
     return response
--- src/utils/auto_insert_book.py
+++ src/utils/auto_insert_book.py
@@ -6,7 +6,7 @@ from os.path import dirname, abspath
 # Añadir el directorio src al sys.path
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
-from models import GenreModel, AuthorModel
+from models import GenreModel, AuthorModel, BookModel
 
 with open("src/mocks/books.json", "r") as file:
   books_data = json.load(file)
@@ -22,13 +22,39 @@ for book_data in books_data:
   for author in book_data["author"]:
     author_set.add(author)
 
-
 # print(genres_set)
 
-for genre in genres_set:
-  GenreModel().post_one_genre(genre)
+# for genre in genres_set:
+#   GenreModel().post_one_genre(genre)
 
-for author in author_set:
-  AuthorModel().post_one_author(author)
+# for author in author_set:
+#   AuthorModel().post_one_author(author)
 
-print(author_set)
+genres = GenreModel().get_all_genre()
+authors = AuthorModel().get_all_author()
+
+print(genres)
+print(authors)
+
+new_books_data = []
+
+for book_data in books_data:
+  book = {
+    "name": book_data["title"],
+    "url_img": "https://localhost:1234",
+    "description": book_data["description"]
+  }
+
+  for author in authors:
+    if (author["name"] in book_data["author"]):
+      book["id_author"] = author["id_author"]
+  
+  new_books_data.append(book)
+
+for book in new_books_data:
+  msg = BookModel().post_one_book(book["name"], book["id_author"], book["description"], book["url_img"])
+  print(msg)
+
+# print(author_set)
+
+# TODO insert into book_genre
