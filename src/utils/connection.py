7d99eabb7740c360d8655bb9631c0c67d5526820
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-05-22 11:16:25 -0500

    correction table author

--- src/utils/connection.py
+++ src/utils/connection.py
@@ -11,9 +11,9 @@ class Database:
     try:
       cursor = db.cursor()
       cursor.execute("""
-        create table if not exists autor
+        create table if not exists author
         (
-          id_autor  int auto_increment
+          id_author  int auto_increment
             primary key,
           name      varchar(200) null
         );
@@ -23,11 +23,11 @@ class Database:
         (
           id_book     int auto_increment
             primary key,
-          id_autor    int  null,
+          id_author    int  null,
           description text not null,
           url_img     text not null,
-          constraint book_autor_id_autor_fk
-            foreign key (id_autor) references autor (id_autor)
+          constraint book_author_id_author_fk
+            foreign key (id_author) references author (id_author)
         );
       """)
       cursor.execute("""
