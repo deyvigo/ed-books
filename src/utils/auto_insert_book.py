7c0eefa68fe47d20c8ce0a6c73a06872741b379f
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-05-23 00:40:41 -0500

    auto-insert de libros terminado

--- src/utils/auto_insert_book.py
+++ src/utils/auto_insert_book.py
@@ -6,7 +6,7 @@ from os.path import dirname, abspath
 # Añadir el directorio src al sys.path
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
-from models import GenreModel, AuthorModel, BookModel
+from models import GenreModel, AuthorModel, BookModel, BookGenreModel
 
 with open("src/mocks/books.json", "r") as file:
   books_data = json.load(file)
@@ -24,17 +24,17 @@ for book_data in books_data:
 
 # print(genres_set)
 
-# for genre in genres_set:
-#   GenreModel().post_one_genre(genre) # <-- insert genre
+for genre in genres_set:
+  GenreModel().post_one_genre(genre) # <-- insert genre
 
-# for author in author_set:
-#   AuthorModel().post_one_author(author) # <-- insert author
+for author in author_set:
+  AuthorModel().post_one_author(author) # <-- insert author
 
 genres = GenreModel().get_all_genre()
 authors = AuthorModel().get_all_author()
 
-print(genres)
-print(authors)
+# print(genres)
+# print(authors)
 
 new_books_data = []
 
@@ -53,8 +53,26 @@ for book_data in books_data:
 
 for book in new_books_data:
   msg = BookModel().post_one_book(book["name"], book["id_author"], book["description"], book["url_img"]) # <-- insert book
-  print(msg)
+#   print(msg)
 
 # print(author_set)
 
-# TODO insert into book_genre
+
+book_genre = []
+
+books = BookModel().get_all_book()
+# print(genres)
+
+for book in books:
+  for book_data in books_data:
+    if (book["name"] == book_data["title"]):
+      # print(f"{book['name']} {book['id_book']}")
+      for genre in genres:
+        if genre["genre_name"] in book_data["genre"]:
+          # print(f"{book['id_book']} {genre['id_genre']}")
+          book_genre.append([book['id_book'], genre['id_genre']])
+
+# print(book_genre)
+
+for data in book_genre:
+  BookGenreModel().post_one_book_genre(data[0], data[1])
