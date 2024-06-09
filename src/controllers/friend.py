1081ddffcd00a9afadf89aa136d66d60de9b436f
BrayanAlq <145175308+BrayanAlq@users.noreply.github.com.>
2024-06-08 21:37:25 -0500

    fix controller and model friend

--- src/controllers/friend.py
+++ src/controllers/friend.py
@@ -5,79 +5,95 @@ from structures.ListaEnlazada import ListaEnlazada
 
 class FriendController:
   @staticmethod
-  def get_all_friends():
-    id = request.args.get('id')
-    response = FriendModel().get_all_friends(id)
-    return response
-
-  @staticmethod
-  def get_all_friends_requests():
-    id = request.args.get('id')
-    if not id:
-        return {"error": "id es requerido"}, 400
-    try:
-        id = int(id)
-    except ValueError:
-        return {"error": "id debe ser un entero"}, 400
-    response = FriendModel().get_received_friends_requests(id)
-    return response
-  
-  @staticmethod
   def post_one_friends_requests():
     data = request.json
-    response = FriendModel().post_one_friend_request(data.get("id_applicant"),data.get("id_receiver"))
+    id_applicant=data.get("id_applicant")
+    id_receiver=data.get("id_receiver")
+    dataFriend = FriendModel().get_all_friend()
+    friends = dataFriend['data']
+    cont=0
+
+    if id_applicant is None or id_receiver is None :
+            return {"error": "id_applicant y id_receiver son requeridos"}, 400
+
+    for friend in friends:
+      if (friend.get("id_applicant") == id_applicant and friend.get("id_receiver") == id_receiver) or (friend.get("id_applicant") == id_receiver and friend.get("id_receiver") == id_applicant):
+        cont+=1
+    if(cont==0):
+       response = FriendModel().post_one_friend_request(data.get("id_applicant"),data.get("id_receiver"))
+    else:
+       return { "error": "Ya existe una solicitud de amistad entre estos usuarios" }, 400
     return response 
   
+  #Actualizar estado de la peticion de amistad // is_accept=-1 (Rechazado)  =0 (pendiente) =1 (aceptado)
   @staticmethod
   def update_friend_status():
     data = request.json
-    response = FriendModel().update_friend_status(data.get("id_applicant"),data.get("id_receiver"),data.get("is_accept"))
+    id_applicant=data.get("id_applicant")
+    id_receiver=data.get("id_receiver")
+    is_accept=data.get("is_accept")
+
+    if id_applicant is None or id_receiver is None or is_accept is None:
+            return {"error": "id_applicant, id_receiver y is_accept son requeridos"}, 400
+
+    if is_accept==-1:
+        response=FriendModel().delete_friend(id_applicant,id_receiver)
+    elif is_accept==0 or is_accept==1:
+        response=FriendModel().update_friend_status(id_applicant,id_receiver,is_accept)
+    else:
+       {"error": f"Error al actualizar la peticion de amistad"}, 500
     return response 
   
   @staticmethod
   def get_friends():
     id = request.args.get('id')
-    data = FriendModel().get_all_friends(id)
+    data = FriendModel().get_all_friend()
     friends = data['data']
     list_friends = ListaDoble()
 
+    if id is None:
+            return {"error": "id es requerido"}, 400
+
     for friend in friends:
+      if (str(friend.get("id_applicant")) == str(id) or str(friend.get("id_receiver")) == str(id)) and str(friend.get("is_accept")) == str(1):
         if str(friend.get("id_applicant")) == str(id):
-            friend_id = friend.get("id_receiver")
+          friend_id = friend.get("id_receiver")
         else:
             friend_id = friend.get("id_applicant")
         list_friends.agregar_al_inicio({"id_friend":friend_id})
-    data=list_friends.viewData()
-    return data 
+    response=list_friends.viewData()
+    return response 
   
   @staticmethod
   def get_received_friends_requests():
     id = request.args.get('id')
-    data= FriendModel().get_received_friends_requests(id)
+    data= FriendModel().get_all_friend()
     requests=data['data'] 
     list_requests= ListaEnlazada()
 
+    if id is None:
+            return {"error": "id es requerido"}, 400
+      
     for req in requests:
-        if str(req.get("id_applicant")) == str(id):
-            request_id = req.get("id_receiver")
-        else:
+        if str(req.get("id_receiver")) == str(id) and str(req.get("is_accept")) == str(0):
             request_id = req.get("id_applicant")
-        list_requests.append({"request_id":request_id})
+            list_requests.append({"request_id":request_id})
     data=list_requests.viewData()
     return data
   
   @staticmethod
   def get_sent_friends_requests():
     id = request.args.get('id')
-    data= FriendModel().get_sent_friends_requests(id)
+    data= FriendModel().get_all_friend()
     requests=data['data'] 
     list_requests= ListaEnlazada()
 
+    if id is None:
+            return {"error": "id es requerido"}, 400
+
     for req in requests:
-        if str(req.get("id_applicant")) == str(id):
+        if str(req.get("id_applicant")) == str(id) and str(req.get("is_accept")) == str(0):
             request_id = req.get("id_receiver")
-        else:
-            request_id = req.get("id_applicant")
-        list_requests.append({"request_id":request_id})
+            list_requests.append({"request_id":request_id})
     data=list_requests.viewData()
     return data
--- src/models/friend.py
+++ src/models/friend.py
@@ -24,95 +24,51 @@ class FriendModel:
             cursor.close()
             self.db.close()
 
-
-    def get_received_friends_requests(self,id):
-        cursor = self.db.cursor()
-        try:
-            query = "SELECT * FROM friend WHERE is_accept = 0 AND id_receiver = %s;"
-            cursor.execute(query, (id))
-            response = cursor.fetchall()
-            return { 'data': response }
-        except:
-            return { "error": "Error al consultar la tabla friend"} 
-        finally:
-            cursor.close()
-            self.db.close()
-
-    #Mostrar solicitudes de amistad
-    def get_sent_friends_requests(self,id):
-        cursor = self.db.cursor()
-        try:
-            query = "SELECT * FROM friend WHERE is_accept = 0 AND id_applicant = %s;"
-            cursor.execute(query, (id))
-            response = cursor.fetchall()
-            return { 'data': response }
-        except:
-            return { "error": "Error al consultar la tabla friend"} 
-        finally:
-            cursor.close()
-            self.db.close()
-
-    #Mostrar amigos
-    def get_all_friends(self,id):
-        cursor = self.db.cursor()
-        try:
-            query = "SELECT * FROM friend WHERE is_accept = 1 AND (id_applicant = %s OR id_receiver=%s);"
-            cursor.execute(query, (id,id))
-            response = cursor.fetchall()
-            return { 'data': response }
-        except:
-            return { "error": "Error al consultar la tabla friend"},500
-        finally:
-            cursor.close()
-            self.db.close()
-
     #Enviar solicitud de amistad // is_accept =0 pendiente
     def post_one_friend_request(self, id_applicant, id_receiver, is_accept=0):
         cursor = self.db.cursor()
-        try:
-            # Verificar si ya existe una solicitud de amistad con las mismas IDs
-            query_check = "SELECT COUNT(*) FROM friend WHERE (id_applicant = %s AND id_receiver = %s) OR (id_applicant = %s AND id_receiver = %s);"
-            cursor.execute(query_check, (id_applicant, id_receiver,id_receiver,id_applicant))
-            result = cursor.fetchone()
-            count = result["COUNT(*)"]            
-            
-            if count > 0:
-                return { "error": "Ya existe una solicitud de amistad entre estos usuarios" }, 400
-            
-            # Si no existe, insertar la nueva solicitud de amistad
+        try:            
             query_insert = "INSERT INTO friend (id_applicant, id_receiver, is_accept) VALUES (%s, %s, %s);"
             cursor.execute(query_insert, (id_applicant, id_receiver, is_accept))
             self.db.commit()
             return { "message": "Solicitud de amistad enviada exitosamente" }, 200
-        except IndexError as ie:
-            print("Error al acceder al resultado de la consulta:", ie)
-            return { "error": "Error al enviar solicitud de amistad" }, 500
         except:
             return { "error": "Error al enviar solicitud de amistad" }, 500
         finally:
             cursor.close()
             self.db.close()
 
-    
-
-    #Actualizar estado de la peticion de amistad // is_accept=-1 (Rechazado)  =0 (pendiente) =1 (aceptado)
     def update_friend_status(self, id_applicant, id_receiver, is_accept):
         cursor = self.db.cursor()
         try:
-            if is_accept == -1:
-                query = "DELETE FROM friend WHERE id_applicant = %s AND id_receiver = %s;"
-                cursor.execute(query, (id_applicant, id_receiver))
-                action = "eliminar"
-            else:
-                query = "UPDATE friend SET is_accept = %s WHERE id_applicant = %s AND id_receiver = %s"
-                cursor.execute(query, (is_accept, id_applicant, id_receiver))
-                action = "actualizar"
+            query = "UPDATE friend SET is_accept = %s WHERE id_applicant = %s AND id_receiver = %s"
+            cursor.execute(query, (is_accept, id_applicant, id_receiver))
+            action = "actualizar"
             self.db.commit()
             if cursor.rowcount == 0:
                 return {"error": f"No hay peticion de amistad para {action}"}, 404
             return {"message": f"Peticion de amistad {action} con éxito"}, 200
         except Exception as e:
             return {"error": f"Error al {action} la peticion de amistad", "details": str(e)}, 500
+        finally:
+            cursor.close()
+            self.db.close()
+    
+    def delete_friend(self, id_applicant, id_receiver):
+        cursor = self.db.cursor()
+        try:
+            query = "DELETE FROM friend WHERE id_applicant = %s AND id_receiver = %s"
+            cursor.execute(query, (id_applicant, id_receiver))
+            self.db.commit()
+
+            if cursor.rowcount == 0:
+                return {"error": "No se encontró la petición de amistad para eliminar"}, 404
+            
+            return {"message": "Petición de amistad eliminada con éxito"}, 200
+
+        except Exception as e:
+            return {"error": "Error al eliminar la petición de amistad", "details": str(e)}, 500
+
         finally:
             cursor.close()
             self.db.close()
