e5cb5a43e84b3a64aaa9991bf251030cb4268563
BrayanAlq <145175308+BrayanAlq@users.noreply.github.com.>
2024-06-28 22:33:07 -0500

    fix merge posts

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,8 +4,5 @@ from models.genre import *
 from models.user import *
 from models.book_genre import *
 from models.friend import *
-<<<<<<< HEAD
 from models.post import *
-=======
 from models.book_user import *
->>>>>>> 4289d2f73440219dca9b85e63855cf95769eda8a
